truenas/linux

Implement native NFSv4 ACLs in NFS server
ZFS currently exposes native ZFS nfsv4 ACL type through
the system.nfs4_acl_xdr xattr. underlying ACL type can
be determined via inode sb flags (previous kernel work).

This PR makes the Kernel NFS server aware of this ACL
type and has it read from and write to the xattr is
the relevant NFS40 ops.

At this point only NFS40 ACLs are implemented (RFC3530)
because they are currently only ones supported by the
Kernel NFS client in Linux.

ACE_INHERITED_ACE is returned in request for NFS40 ACL.
This is for consistency with FreeBSD behavior, and is
important for SMB / NFS compatibility. At a future
point in time, full NFS41 ACL compatibility will
most likely be required (for feature parity with
other commercial vendors). nfs4-acl-tools version 0.3.3
and earlier contains a bug where the persence of
ACE_INHERITED_ACE is mistakenly identified as making
ACE apply to the file owner.

Signed-off-by: Andrew Walker <awalker@ixsystems.com>

fs/nfsd/nfs4acl.c
@@ -37,11 +37,13 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/posix_acl.h>
+#include <linux/xattr.h>
 
 #include "nfsfh.h"
 #include "nfsd.h"
 #include "acl.h"
 #include "vfs.h"
+#include "nfs41acl_xdr.h"
 
 #define NFS4_ACL_TYPE_DEFAULT	0x01
 #define NFS4_ACL_DIR		0x02
@@ -125,8 +127,8 @@ static short ace2type(struct nfs4_ace *);
 static void _posix_to_nfsv4_one(struct posix_acl *, struct nfs4_acl *,
 				unsigned int);
 
-int
-nfsd4_get_nfs4_acl(struct svc_rqst *rqstp, struct dentry *dentry,
+static int
+get_nfs4_posix_acl(struct svc_rqst *rqstp, struct dentry *dentry,
 		struct nfs4_acl **acl)
 {
 	struct inode *inode = d_inode(dentry);
@@ -174,6 +176,161 @@ out:
 rel_pacl:
 	posix_acl_release(pacl);
 	return error;
+}
+
+static int
+convert_to_nfs40_ace(u32 *xdrbuf, struct nfs4_ace *ace)
+{
+	int error = 0;
+	u32 iflag, id;
+
+	ace->type = ntohl(*(xdrbuf++));
+	if (ace->type > NFS4_ACE_ACCESS_DENIED_ACE_TYPE)
+		return -EINVAL;
+
+	ace->flag = ntohl(*(xdrbuf++));
+	iflag = ntohl(*(xdrbuf++));
+	ace->access_mask = ntohl(*(xdrbuf++)) & NFS4_ACE_MASK_ALL;
+	id = ntohl(*(xdrbuf++));
+
+	if (iflag & ACEI4_SPECIAL_WHO) {
+		switch(id) {
+		case ACE4_SPECIAL_OWNER:
+			ace->whotype = NFS4_ACL_WHO_OWNER;
+			break;
+		case ACE4_SPECIAL_GROUP:
+			ace->whotype = NFS4_ACL_WHO_GROUP;
+			break;
+		case ACE4_SPECIAL_EVERYONE:
+			ace->whotype = NFS4_ACL_WHO_EVERYONE;
+			break;
+		}
+	} else {
+		ace->whotype = NFS4_ACL_WHO_NAMED;
+		if (ace->flag & NFS4_ACE_IDENTIFIER_GROUP) {
+			ace->who_gid = make_kgid(&init_user_ns, id);
+			if (!gid_valid(ace->who_gid)) {
+				error = -EINVAL;
+			}
+		} else {
+			ace->who_uid = make_kuid(&init_user_ns, id);
+			if (!uid_valid(ace->who_uid)) {
+				error = -EINVAL;
+			}
+		}
+	}
+
+	return error;
+}
+
+static int
+convert_nfs41xdr_to_nfs40_acl(u32 *xdrbuf, size_t remaining, struct nfs4_acl *acl)
+{
+	int error = 0;
+	int i;
+
+	for (i = 0; i < acl->naces; i++, xdrbuf += NACE41_LEN) {
+		if (remaining < (NACE41_LEN * sizeof(u32))) {
+			error = -EOVERFLOW;
+			break;
+		}
+
+		error = convert_to_nfs40_ace(xdrbuf, &acl->aces[i]);
+		if (error)
+			break;
+
+		remaining -= (NACE41_LEN * sizeof(u32));
+	}
+
+	return error;
+}
+
+static int
+get_nfs4_nfsv41xdr_acl(struct svc_rqst *rqstp, struct dentry *dentry,
+		struct nfs4_acl **pacl)
+{
+	int error = 0;
+	u32 ace_cnt;
+	u32 *xdr_buf = NULL, *p;
+	struct nfs4_acl *acl = NULL;
+	ssize_t len;
+	size_t xdr_buf_sz = ACES_TO_XDRSIZE(NFS41ACL_MAX_ENTRIES);
+
+	xdr_buf = kzalloc(xdr_buf_sz, GFP_KERNEL);
+	if (!xdr_buf)
+		return -ENOMEM;
+
+	len = vfs_getxattr(&init_user_ns, dentry, NA41_NAME, xdr_buf, xdr_buf_sz);
+	if (len == 0) {
+		error = -EOPNOTSUPP;
+		goto out;
+	}
+
+	if (len < 0) {
+		switch (len) {
+		case -EOPNOTSUPP:
+			/* ZFS says NFSv4 ACLs not supported */
+			error = -EOPNOTSUPP;
+			goto out;
+		case -EINVAL:
+			/* ZFS unhappy with buffer size */
+			error = -EINVAL;
+			goto out;
+		case -ERANGE:
+			/* our buffer is too small. This is _very_ unexpected */
+			error = -EINVAL;
+			goto out;
+		case -EPERM:
+		case -EACCES:
+			error = -EPERM;
+			goto out;
+		default:
+			error = -ENOMEM;
+			goto out;
+		}
+	}
+
+	BUG_ON(!(XDRSIZE_IS_VALID(len)));
+
+	/*
+	 * At preset only NFS 4.0 (RFC 3530) ACLs are exported by the NFS
+	 * server, and so the ACL-wide flags are ignored when generating the
+	 * internal NFS server ACL.
+	 */
+	p = xdr_buf + 1;
+	ace_cnt = ntohl(*(p++));
+	if (ace_cnt > NFS41ACL_MAX_ENTRIES) {
+		error = -ERANGE;
+		goto out;
+	}
+
+	acl = kzalloc(nfs4_acl_bytes(ace_cnt), GFP_KERNEL);
+	if (!acl) {
+		error = -ENOMEM;
+		goto out;
+	}
+	acl->naces = ace_cnt;
+
+	error = convert_nfs41xdr_to_nfs40_acl(p++, len - (2 * sizeof(u32)), acl);
+	if (error)
+		kfree(acl);
+	else
+		*pacl = acl;
+out:
+	kfree(xdr_buf);
+	return (error);
+}
+
+int
+nfsd4_get_nfs4_acl(struct svc_rqst *rqstp, struct dentry *dentry,
+		struct nfs4_acl **acl)
+{
+	struct inode *inode = d_inode(dentry);
+
+	if (IS_NFSV4ACL(inode))
+		return get_nfs4_nfsv41xdr_acl(rqstp, dentry, acl);
+	else
+		return get_nfs4_posix_acl(rqstp, dentry, acl);
 }
 
 struct posix_acl_summary {
@@ -751,24 +908,148 @@ out_estate:
 	return ret;
 }
 
-__be32 nfsd4_acl_to_attr(enum nfs_ftype4 type, struct nfs4_acl *acl,
-			 struct nfsd_attrs *attr)
+static __be32
+nfsd4_acl_to_attr_posix(enum nfs_ftype4 type, struct nfs4_acl *acl,
+			fsacl_t *fsaclp)
 {
 	int host_error;
 	unsigned int flags = 0;
 
 	if (!acl)
 		return nfs_ok;
-
 	if (type == NF4DIR)
 		flags = NFS4_ACL_DIR;
 
-	host_error = nfs4_acl_nfsv4_to_posix(acl, &attr->na_pacl,
-					     &attr->na_dpacl, flags);
+	host_error = nfs4_acl_nfsv4_to_posix(acl, &fsaclp->posixacl.na_pacl,
+					     &fsaclp->posixacl.na_dpacl, flags);
 	if (host_error == -EINVAL)
 		return nfserr_attrnotsupp;
 	else
 		return nfserrno(host_error);
+}
+
+static int
+convert_ace_to_nfs41(u32 *p, const struct nfs4_ace *ace)
+{
+	int error = 0;
+	u32 iflag = 0, who = -1;
+
+	/* Audit and Alarm are not currently supported */
+	if (ace->type > NFS4_ACE_ACCESS_DENIED_ACE_TYPE)
+		return -EINVAL;
+
+	switch (ace->whotype) {
+	case NFS4_ACL_WHO_OWNER:
+		iflag = ACEI4_SPECIAL_WHO;
+		who = ACE4_SPECIAL_OWNER;
+		break;
+	case NFS4_ACL_WHO_GROUP:
+		iflag = ACEI4_SPECIAL_WHO;
+		who = ACE4_SPECIAL_GROUP;
+		break;
+	case NFS4_ACL_WHO_EVERYONE:
+		iflag = ACEI4_SPECIAL_WHO;
+		who = ACE4_SPECIAL_EVERYONE;
+		break;
+	case NFS4_ACL_WHO_NAMED:
+		if (ace->flag & NFS4_ACE_IDENTIFIER_GROUP)
+			who = (u32)__kgid_val(ace->who_gid);
+		else
+			who = (u32)__kuid_val(ace->who_uid);
+		break;
+	default:
+		error = -EINVAL;
+	}
+
+	*p++ = htonl(ace->type);
+	*p++ = htonl(ace->flag & NFS41_FLAGS);
+	*p++ = htonl(iflag);
+	*p++ = htonl(ace->access_mask & NFS4_ACE_MASK_ALL);
+	*p++ = htonl(who);
+
+	return error;
+}
+
+static int
+generate_nfs41acl_buf(u32 *xdrbuf, const struct nfs4_acl *acl)
+{
+	int error = 0;
+	int i;
+
+	/* first byte is NFS41 Flags. Skip since these are RFC3530 acls */
+	*xdrbuf++ = 0;
+	*xdrbuf++ = htonl(acl->naces);
+
+	for (i = 0; i < acl->naces; i++, xdrbuf += NACE41_LEN) {
+		error = convert_ace_to_nfs41(xdrbuf, &acl->aces[i]);
+		if (error)
+			break;
+	}
+
+	return error;
+}
+
+static __be32
+nfsd4_acl_to_attr_zfsacl(enum nfs_ftype4 type, struct nfs4_acl *acl,
+			 fsacl_t *fsaclp)
+{
+	int error;
+	u32 *xdr_buf = NULL;
+	size_t len;
+
+	if (!acl)
+		return nfs_ok;
+
+	if (acl->naces > NFS41ACL_MAX_ENTRIES)
+		return nfserrno(-ERANGE);
+
+	else if (acl->naces == 0)
+		return nfserrno(-EINVAL);
+
+	len = ACES_TO_XDRSIZE(acl->naces);
+
+	xdr_buf = kzalloc(len, GFP_KERNEL);
+	if (!xdr_buf)
+		return nfserrno(-ENOMEM);
+
+	error = generate_nfs41acl_buf(xdr_buf, acl);
+	if (error) {
+		kfree(xdr_buf);
+		return nfserrno(error);
+	}
+
+	fsaclp->zfsacl.aclbuf = xdr_buf;
+	fsaclp->zfsacl.sz = len;
+
+	return nfs_ok;
+}
+
+static __be32
+nfsd4_acl_to_attr_fail(enum nfs_ftype4 type, struct nfs4_acl *acl,
+		       fsacl_t *fsaclp)
+{
+	if (!acl)
+		return nfs_ok;
+
+	return nfserr_attrnotsupp;
+}
+
+int
+nfsv4_set_zfacl_from_attr(struct dentry *dentry, struct nfsd_attrs *attr)
+{
+	struct inode *delegated_inode = NULL;
+	int error;
+
+retry:
+	error = __vfs_setxattr_locked(&init_user_ns, dentry, NA41_NAME,
+				      attr->na_fsacl.zfsacl.aclbuf,
+				      attr->na_fsacl.zfsacl.sz,
+				      XATTR_REPLACE, &delegated_inode);
+
+	if (delegated_inode)
+		goto retry;
+
+	return error;
 }
 
 static short
@@ -850,4 +1131,21 @@ __be32 nfs4_acl_write_who(struct xdr_stream *xdr, int who)
 	}
 	WARN_ON_ONCE(1);
 	return nfserr_serverfault;
+}
+
+void
+nfsd4_setup_attr(struct dentry *dentry, struct nfsd_attrs *attr)
+{
+	struct inode *inode = d_inode(dentry);
+
+	if (IS_NFSV4ACL(inode)) {
+		attr->na_acltype = ACL_TYPE_ZFS;
+		attr->na_conv_fn = nfsd4_acl_to_attr_zfsacl;
+	} else if (IS_POSIXACL(inode)) {
+		attr->na_acltype = ACL_TYPE_POSIX;
+		attr->na_conv_fn = nfsd4_acl_to_attr_posix;
+	} else {
+		attr->na_acltype = ACL_TYPE_NONE;
+		attr->na_conv_fn = nfsd4_acl_to_attr_fail;
+	}
 }

fs/nfsd/vfs.c
@@ -478,15 +478,29 @@ nfsd_setattr(struct svc_rqst *rqstp, struct svc_fh *fhp,
 	if (attr->na_seclabel && attr->na_seclabel->len)
 		attr->na_labelerr = security_inode_setsecctx(dentry,
 			attr->na_seclabel->data, attr->na_seclabel->len);
-	if (IS_ENABLED(CONFIG_FS_POSIX_ACL) && attr->na_pacl)
-		attr->na_aclerr = set_posix_acl(&init_user_ns,
-						inode, ACL_TYPE_ACCESS,
-						attr->na_pacl);
-	if (IS_ENABLED(CONFIG_FS_POSIX_ACL) &&
-	    !attr->na_aclerr && attr->na_dpacl && S_ISDIR(inode->i_mode))
-		attr->na_aclerr = set_posix_acl(&init_user_ns,
-						inode, ACL_TYPE_DEFAULT,
-						attr->na_dpacl);
+
+	switch(attr->na_acltype) {
+	case ACL_TYPE_POSIX:
+		if (IS_ENABLED(CONFIG_FS_POSIX_ACL) && attr->na_fsacl.posixacl.na_pacl)
+			attr->na_aclerr = set_posix_acl(&init_user_ns,
+							inode, ACL_TYPE_ACCESS,
+							attr->na_fsacl.posixacl.na_pacl);
+		if (IS_ENABLED(CONFIG_FS_POSIX_ACL) &&
+		    !attr->na_aclerr && attr->na_fsacl.posixacl.na_dpacl &&
+                    S_ISDIR(inode->i_mode))
+			attr->na_aclerr = set_posix_acl(&init_user_ns,
+							inode, ACL_TYPE_DEFAULT,
+							attr->na_fsacl.posixacl.na_dpacl);
+		break;
+	case ACL_TYPE_ZFS:
+		if (attr->na_fsacl.zfsacl.aclbuf)
+			attr->na_aclerr = nfsv4_set_zfacl_from_attr(dentry, attr);
+		break;
+	case ACL_TYPE_NONE:
+		break;
+	default:
+		BUG();
+	};
 	inode_unlock(inode);
 	if (size_change)
 		put_write_access(inode);
@@ -1303,7 +1317,7 @@ nfsd_create_locked(struct svc_rqst *rqstp, struct svc_fh *fhp,
 		iap->ia_mode = 0;
 	iap->ia_mode = (iap->ia_mode & S_IALLUGO) | type;
 
-	if (!IS_POSIXACL(dirp))
+	if (!IS_POSIXACL(dirp) && !IS_NFSV4ACL(dirp))
 		iap->ia_mode &= ~current_umask();
 
 	err = 0;
@@ -2327,6 +2341,20 @@ nfsd_permission(struct svc_rqst *rqstp, struct svc_export *exp,
 	/* This assumes  NFSD_MAY_{READ,WRITE,EXEC} == MAY_{READ,WRITE,EXEC} */
 	err = inode_permission(&init_user_ns, inode,
 			       acc & (MAY_READ | MAY_WRITE | MAY_EXEC));
+
+	/*
+	 * See RFC 5661 Section 6.2.1.3.2
+	 * Allow NFSv4 ACL to override normal delete permission
+	 * In this case REMOVE is granted if DELETE is granted on file
+	 * or DELETE_CHILD is granted on parent.
+	 */
+	if ((err == -EACCES) && IS_NFSV4ACL(inode) &&
+	    (acc == NFSD_MAY_REMOVE)) {
+		err = inode_permission(&init_user_ns, inode, MAY_DELETE);
+		if (err == -EACCES)
+			err = inode_permission(&init_user_ns, d_inode(dentry->d_parent),
+			    MAY_DELETE_CHILD);
+	}
 
 	/* Allow read access to binaries even when mode 111 */
 	if (err == -EACCES && S_ISREG(inode->i_mode) &&

wongsyrone/truenas-scale-linux

fs/nfsd/nfs4acl.c
@@ -37,11 +37,13 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/posix_acl.h>
+#include <linux/xattr.h>
 
 #include "nfsfh.h"
 #include "nfsd.h"
 #include "acl.h"
 #include "vfs.h"
+#include "nfs41acl_xdr.h"
 
 #define NFS4_ACL_TYPE_DEFAULT	0x01
 #define NFS4_ACL_DIR		0x02
@@ -125,8 +127,8 @@ static short ace2type(struct nfs4_ace *);
 static void _posix_to_nfsv4_one(struct posix_acl *, struct nfs4_acl *,
 				unsigned int);
 
-int
-nfsd4_get_nfs4_acl(struct svc_rqst *rqstp, struct dentry *dentry,
+static int
+get_nfs4_posix_acl(struct svc_rqst *rqstp, struct dentry *dentry,
 		struct nfs4_acl **acl)
 {
 	struct inode *inode = d_inode(dentry);
@@ -174,6 +176,161 @@ out:
 rel_pacl:
 	posix_acl_release(pacl);
 	return error;
+}
+
+static int
+convert_to_nfs40_ace(u32 *xdrbuf, struct nfs4_ace *ace)
+{
+	int error = 0;
+	u32 iflag, id;
+
+	ace->type = ntohl(*(xdrbuf++));
+	if (ace->type > NFS4_ACE_ACCESS_DENIED_ACE_TYPE)
+		return -EINVAL;
+
+	ace->flag = ntohl(*(xdrbuf++));
+	iflag = ntohl(*(xdrbuf++));
+	ace->access_mask = ntohl(*(xdrbuf++)) & NFS4_ACE_MASK_ALL;
+	id = ntohl(*(xdrbuf++));
+
+	if (iflag & ACEI4_SPECIAL_WHO) {
+		switch(id) {
+		case ACE4_SPECIAL_OWNER:
+			ace->whotype = NFS4_ACL_WHO_OWNER;
+			break;
+		case ACE4_SPECIAL_GROUP:
+			ace->whotype = NFS4_ACL_WHO_GROUP;
+			break;
+		case ACE4_SPECIAL_EVERYONE:
+			ace->whotype = NFS4_ACL_WHO_EVERYONE;
+			break;
+		}
+	} else {
+		ace->whotype = NFS4_ACL_WHO_NAMED;
+		if (ace->flag & NFS4_ACE_IDENTIFIER_GROUP) {
+			ace->who_gid = make_kgid(&init_user_ns, id);
+			if (!gid_valid(ace->who_gid)) {
+				error = -EINVAL;
+			}
+		} else {
+			ace->who_uid = make_kuid(&init_user_ns, id);
+			if (!uid_valid(ace->who_uid)) {
+				error = -EINVAL;
+			}
+		}
+	}
+
+	return error;
+}
+
+static int
+convert_nfs41xdr_to_nfs40_acl(u32 *xdrbuf, size_t remaining, struct nfs4_acl *acl)
+{
+	int error = 0;
+	int i;
+
+	for (i = 0; i < acl->naces; i++, xdrbuf += NACE41_LEN) {
+		if (remaining < (NACE41_LEN * sizeof(u32))) {
+			error = -EOVERFLOW;
+			break;
+		}
+
+		error = convert_to_nfs40_ace(xdrbuf, &acl->aces[i]);
+		if (error)
+			break;
+
+		remaining -= (NACE41_LEN * sizeof(u32));
+	}
+
+	return error;
+}
+
+static int
+get_nfs4_nfsv41xdr_acl(struct svc_rqst *rqstp, struct dentry *dentry,
+		struct nfs4_acl **pacl)
+{
+	int error = 0;
+	u32 ace_cnt;
+	u32 *xdr_buf = NULL, *p;
+	struct nfs4_acl *acl = NULL;
+	ssize_t len;
+	size_t xdr_buf_sz = ACES_TO_XDRSIZE(NFS41ACL_MAX_ENTRIES);
+
+	xdr_buf = kzalloc(xdr_buf_sz, GFP_KERNEL);
+	if (!xdr_buf)
+		return -ENOMEM;
+
+	len = vfs_getxattr(&nop_mnt_idmap, dentry, NA41_NAME, xdr_buf, xdr_buf_sz);
+	if (len == 0) {
+		error = -EOPNOTSUPP;
+		goto out;
+	}
+
+	if (len < 0) {
+		switch (len) {
+		case -EOPNOTSUPP:
+			/* ZFS says NFSv4 ACLs not supported */
+			error = -EOPNOTSUPP;
+			goto out;
+		case -EINVAL:
+			/* ZFS unhappy with buffer size */
+			error = -EINVAL;
+			goto out;
+		case -ERANGE:
+			/* our buffer is too small. This is _very_ unexpected */
+			error = -EINVAL;
+			goto out;
+		case -EPERM:
+		case -EACCES:
+			error = -EPERM;
+			goto out;
+		default:
+			error = -ENOMEM;
+			goto out;
+		}
+	}
+
+	BUG_ON(!(XDRSIZE_IS_VALID(len)));
+
+	/*
+	 * At preset only NFS 4.0 (RFC 3530) ACLs are exported by the NFS
+	 * server, and so the ACL-wide flags are ignored when generating the
+	 * internal NFS server ACL.
+	 */
+	p = xdr_buf + 1;
+	ace_cnt = ntohl(*(p++));
+	if (ace_cnt > NFS41ACL_MAX_ENTRIES) {
+		error = -ERANGE;
+		goto out;
+	}
+
+	acl = kzalloc(nfs4_acl_bytes(ace_cnt), GFP_KERNEL);
+	if (!acl) {
+		error = -ENOMEM;
+		goto out;
+	}
+	acl->naces = ace_cnt;
+
+	error = convert_nfs41xdr_to_nfs40_acl(p++, len - (2 * sizeof(u32)), acl);
+	if (error)
+		kfree(acl);
+	else
+		*pacl = acl;
+out:
+	kfree(xdr_buf);
+	return (error);
+}
+
+int
+nfsd4_get_nfs4_acl(struct svc_rqst *rqstp, struct dentry *dentry,
+		struct nfs4_acl **acl)
+{
+	struct inode *inode = d_inode(dentry);
+
+	if (IS_NFSV4ACL(inode))
+		return get_nfs4_nfsv41xdr_acl(rqstp, dentry, acl);
+	else
+		return get_nfs4_posix_acl(rqstp, dentry, acl);
 }
 
 struct posix_acl_summary {
@@ -775,24 +932,148 @@ out_estate:
 	return ret;
 }
 
-__be32 nfsd4_acl_to_attr(enum nfs_ftype4 type, struct nfs4_acl *acl,
-			 struct nfsd_attrs *attr)
+static __be32
+nfsd4_acl_to_attr_posix(enum nfs_ftype4 type, struct nfs4_acl *acl,
+			fsacl_t *fsaclp)
 {
 	int host_error;
 	unsigned int flags = 0;
 
 	if (!acl)
 		return nfs_ok;
-
 	if (type == NF4DIR)
 		flags = NFS4_ACL_DIR;
 
-	host_error = nfs4_acl_nfsv4_to_posix(acl, &attr->na_pacl,
-					     &attr->na_dpacl, flags);
+	host_error = nfs4_acl_nfsv4_to_posix(acl, &fsaclp->posixacl.na_pacl,
+					     &fsaclp->posixacl.na_dpacl, flags);
 	if (host_error == -EINVAL)
 		return nfserr_attrnotsupp;
 	else
 		return nfserrno(host_error);
+}
+
+static int
+convert_ace_to_nfs41(u32 *p, const struct nfs4_ace *ace)
+{
+	int error = 0;
+	u32 iflag = 0, who = -1;
+
+	/* Audit and Alarm are not currently supported */
+	if (ace->type > NFS4_ACE_ACCESS_DENIED_ACE_TYPE)
+		return -EINVAL;
+
+	switch (ace->whotype) {
+	case NFS4_ACL_WHO_OWNER:
+		iflag = ACEI4_SPECIAL_WHO;
+		who = ACE4_SPECIAL_OWNER;
+		break;
+	case NFS4_ACL_WHO_GROUP:
+		iflag = ACEI4_SPECIAL_WHO;
+		who = ACE4_SPECIAL_GROUP;
+		break;
+	case NFS4_ACL_WHO_EVERYONE:
+		iflag = ACEI4_SPECIAL_WHO;
+		who = ACE4_SPECIAL_EVERYONE;
+		break;
+	case NFS4_ACL_WHO_NAMED:
+		if (ace->flag & NFS4_ACE_IDENTIFIER_GROUP)
+			who = (u32)__kgid_val(ace->who_gid);
+		else
+			who = (u32)__kuid_val(ace->who_uid);
+		break;
+	default:
+		error = -EINVAL;
+	}
+
+	*p++ = htonl(ace->type);
+	*p++ = htonl(ace->flag & NFS41_FLAGS);
+	*p++ = htonl(iflag);
+	*p++ = htonl(ace->access_mask & NFS4_ACE_MASK_ALL);
+	*p++ = htonl(who);
+
+	return error;
+}
+
+static int
+generate_nfs41acl_buf(u32 *xdrbuf, const struct nfs4_acl *acl)
+{
+	int error = 0;
+	int i;
+
+	/* first byte is NFS41 Flags. Skip since these are RFC3530 acls */
+	*xdrbuf++ = 0;
+	*xdrbuf++ = htonl(acl->naces);
+
+	for (i = 0; i < acl->naces; i++, xdrbuf += NACE41_LEN) {
+		error = convert_ace_to_nfs41(xdrbuf, &acl->aces[i]);
+		if (error)
+			break;
+	}
+
+	return error;
+}
+
+static __be32
+nfsd4_acl_to_attr_zfsacl(enum nfs_ftype4 type, struct nfs4_acl *acl,
+			 fsacl_t *fsaclp)
+{
+	int error;
+	u32 *xdr_buf = NULL;
+	size_t len;
+
+	if (!acl)
+		return nfs_ok;
+
+	if (acl->naces > NFS41ACL_MAX_ENTRIES)
+		return nfserrno(-ERANGE);
+
+	else if (acl->naces == 0)
+		return nfserrno(-EINVAL);
+
+	len = ACES_TO_XDRSIZE(acl->naces);
+
+	xdr_buf = kzalloc(len, GFP_KERNEL);
+	if (!xdr_buf)
+		return nfserrno(-ENOMEM);
+
+	error = generate_nfs41acl_buf(xdr_buf, acl);
+	if (error) {
+		kfree(xdr_buf);
+		return nfserrno(error);
+	}
+
+	fsaclp->zfsacl.aclbuf = xdr_buf;
+	fsaclp->zfsacl.sz = len;
+
+	return nfs_ok;
+}
+
+static __be32
+nfsd4_acl_to_attr_fail(enum nfs_ftype4 type, struct nfs4_acl *acl,
+		       fsacl_t *fsaclp)
+{
+	if (!acl)
+		return nfs_ok;
+
+	return nfserr_attrnotsupp;
+}
+
+int
+nfsv4_set_zfacl_from_attr(struct dentry *dentry, struct nfsd_attrs *attr)
+{
+	struct inode *delegated_inode = NULL;
+	int error;
+
+retry:
+	error = __vfs_setxattr_locked(&nop_mnt_idmap, dentry, NA41_NAME,
+				      attr->na_fsacl.zfsacl.aclbuf,
+				      attr->na_fsacl.zfsacl.sz,
+				      XATTR_REPLACE, &delegated_inode);
+
+	if (delegated_inode)
+		goto retry;
+
+	return error;
 }
 
 static short
@@ -874,4 +1155,21 @@ __be32 nfs4_acl_write_who(struct xdr_stream *xdr, int who)
 	}
 	WARN_ON_ONCE(1);
 	return nfserr_serverfault;
+}
+
+void
+nfsd4_setup_attr(struct dentry *dentry, struct nfsd_attrs *attr)
+{
+	struct inode *inode = d_inode(dentry);
+
+	if (IS_NFSV4ACL(inode)) {
+		attr->na_acltype = ACL_TYPE_ZFS;
+		attr->na_conv_fn = nfsd4_acl_to_attr_zfsacl;
+	} else if (IS_POSIXACL(inode)) {
+		attr->na_acltype = ACL_TYPE_POSIX;
+		attr->na_conv_fn = nfsd4_acl_to_attr_posix;
+	} else {
+		attr->na_acltype = ACL_TYPE_NONE;
+		attr->na_conv_fn = nfsd4_acl_to_attr_fail;
+	}
 }

fs/nfsd/vfs.c
@@ -582,15 +582,30 @@ nfsd_setattr(struct svc_rqst *rqstp, struct svc_fh *fhp,
 	if (attr->na_seclabel && attr->na_seclabel->len)
 		attr->na_labelerr = security_inode_setsecctx(dentry,
 			attr->na_seclabel->data, attr->na_seclabel->len);
-	if (IS_ENABLED(CONFIG_FS_POSIX_ACL) && attr->na_pacl)
-		attr->na_aclerr = set_posix_acl(&nop_mnt_idmap,
-						dentry, ACL_TYPE_ACCESS,
-						attr->na_pacl);
-	if (IS_ENABLED(CONFIG_FS_POSIX_ACL) &&
-	    !attr->na_aclerr && attr->na_dpacl && S_ISDIR(inode->i_mode))
-		attr->na_aclerr = set_posix_acl(&nop_mnt_idmap,
-						dentry, ACL_TYPE_DEFAULT,
-						attr->na_dpacl);
+
+	switch(attr->na_acltype) {
+	case ACL_TYPE_POSIX:
+		if (IS_ENABLED(CONFIG_FS_POSIX_ACL) && attr->na_fsacl.posixacl.na_pacl)
+			attr->na_aclerr = set_posix_acl(&nop_mnt_idmap,
+							dentry, ACL_TYPE_ACCESS,
+							attr->na_fsacl.posixacl.na_pacl);
+		if (IS_ENABLED(CONFIG_FS_POSIX_ACL) &&
+		    !attr->na_aclerr && attr->na_fsacl.posixacl.na_dpacl &&
+			S_ISDIR(inode->i_mode))
+			attr->na_aclerr = set_posix_acl(&nop_mnt_idmap,
+							dentry, ACL_TYPE_DEFAULT,
+							attr->na_fsacl.posixacl.na_dpacl);
+		break;
+	case ACL_TYPE_ZFS:
+		if (attr->na_fsacl.zfsacl.aclbuf)
+			attr->na_aclerr = nfsv4_set_zfacl_from_attr(dentry, attr);
+		break;
+	case ACL_TYPE_NONE:
+		break;
+	default:
+		BUG();
+	};
+
 out_fill_attrs:
 	/*
 	 * RFC 1813 Section 3.3.2 does not mandate that an NFS server
@@ -1480,7 +1495,7 @@ nfsd_create_locked(struct svc_rqst *rqstp, struct svc_fh *fhp,
 		iap->ia_mode = 0;
 	iap->ia_mode = (iap->ia_mode & S_IALLUGO) | type;
 
-	if (!IS_POSIXACL(dirp))
+	if (!IS_POSIXACL(dirp) && !IS_NFSV4ACL(dirp))
 		iap->ia_mode &= ~current_umask();
 
 	err = 0;
@@ -2565,6 +2580,20 @@ nfsd_permission(struct svc_cred *cred, struct svc_export *exp,
 	/* This assumes  NFSD_MAY_{READ,WRITE,EXEC} == MAY_{READ,WRITE,EXEC} */
 	err = inode_permission(&nop_mnt_idmap, inode,
 			       acc & (MAY_READ | MAY_WRITE | MAY_EXEC));
+
+	/*
+	 * See RFC 5661 Section 6.2.1.3.2
+	 * Allow NFSv4 ACL to override normal delete permission
+	 * In this case REMOVE is granted if DELETE is granted on file
+	 * or DELETE_CHILD is granted on parent.
+	 */
+	if ((err == -EACCES) && IS_NFSV4ACL(inode) &&
+	    (acc == NFSD_MAY_REMOVE)) {
+		err = inode_permission(&nop_mnt_idmap, inode, MAY_DELETE);
+		if (err == -EACCES)
+			err = inode_permission(&nop_mnt_idmap, d_inode(dentry->d_parent),
+			    MAY_DELETE_CHILD);
+	}
 
 	/* Allow read access to binaries even when mode 111 */
 	if (err == -EACCES && S_ISREG(inode->i_mode) &&